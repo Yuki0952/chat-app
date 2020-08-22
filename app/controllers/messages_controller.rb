4548c3589e4d6b16054c6613c2d8e2c14308f723
Yuki0952 <69298559+Yuki0952@users.noreply.github.com>
2020-08-22 10:55:57 +0900

    画面表示の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,10 +9,10 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      redirect_to room_messages_path(@room) #参加しているチャットルームに投稿したメッセージの一覧画面にリダイレクト(自動で転送)
+      redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
-      render :index　　#失敗した場合、indexアクションが実行され、同じページに戻る
+      render :index
     end
   end
 
